69919b342477e409a3d8ddb41bb749af3536b3ea
iamdeja <iamdeja@protonmail.ch>
2020-03-14 09:56:56 +0200

    Add file handle display

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 Attempt at CLI tool to list, track and manipulate processes used by a file.
 
-Interfacing with the processes is based on Microsoft's Process Status API [MS DOCS](https://docs.microsoft.com/en-us/windows/win32/psapi/process-status-helper).
+Interfacing with the processes is based on Microsoft's Process Status API on [MS DOCS](https://docs.microsoft.com/en-us/windows/win32/psapi/process-status-helper).
 
-The structures used are based on the invaluable list of _NtQuerySystemInformation_ structures documented in exploit-monday's [blog post](http://www.exploit-monday.com/2013/06/undocumented-ntquerysysteminformation.html). More information can be found on Microsoft's documentation on [NtQuerySystemInformation](https://docs.microsoft.com/en-us/windows/win32/api/winternl/nf-winternl-ntquerysysteminformation) and [ZwQuerySystemInformation](https://docs.microsoft.com/en-us/windows/win32/sysinfo/zwquerysysteminformation).
+The structures used are based on the invaluable list of _NtQuerySystemInformation_ structures documented in exploit-monday's [blog post](http://www.exploit-monday.com/2013/06/undocumented-ntquerysysteminformation.html). More information can be found in Microsoft's documentation on [NtQuerySystemInformation](https://docs.microsoft.com/en-us/windows/win32/api/winternl/nf-winternl-ntquerysysteminformation) and [ZwQuerySystemInformation](https://docs.microsoft.com/en-us/windows/win32/sysinfo/zwquerysysteminformation).
 
-Geoff Chappell has resources concerning Kernel level functions on his [website](https://www.geoffchappell.com/studies/windows/km/index.htm). For some more deeper research, some undocumented functions can be found [here](http://undocumented.ntinternals.net/index.html?page=UserMode%2FUndocumented%20Functions%2FSystem%20Information%2FStructures%2FSYSTEM_PROCESS_INFORMATION.html).
+Geoff Chappell has resources concerning Kernel level functions on his [website](https://www.geoffchappell.com/studies/windows/km/index.htm). For deeper research, some undocumented functions can be found [here](http://undocumented.ntinternals.net/index.html?page=UserMode%2FUndocumented%20Functions%2FSystem%20Information%2FStructures%2FSYSTEM_PROCESS_INFORMATION.html).
+
+Enumeration logic based on the psutil [source](https://github.com/tamentis/psutil/blob/master/psutil/arch/mswindows/process_handles.c).
--- main.c
+++ main.c
@@ -7,12 +7,15 @@
 #include <string.h>
 
 #define SUCCESS 0
-#define MEMFL 1	// memory allocation failed
+#define MEMFL 1 // memory allocation failed
 #define SNAPFL 2 // snapshot capturing failed
 #define SRCHFL 3 // search failed
+#define NULLPTR 13 // dereferences null pointer
 int err_code = SUCCESS;
 
+#ifndef NT_SUCCESS
 #define NT_SUCCESS(x) ((x) >= 0)
+#endif
 #define STATUS_INFO_LENGTH_MISMATCH 0xc0000004
 
 #define SystemHandleInformation 16
@@ -20,38 +23,40 @@ int err_code = SUCCESS;
 #define ObjectNameInformation 1
 #define ObjectTypeInformation 2
 
-int validateInput(TCHAR *, int *);
+int validateInput(TCHAR*, int*);
 int PrintModules(DWORD);
 int retrieveHandleCount(HANDLE);
 
-typedef NTSTATUS(NTAPI *_NtQuerySystemInformation)(
-		ULONG SystemInformationClass,
-		PVOID SystemInformation,
-		ULONG SystemInformationLength,
-		PULONG ReturnLength);
+typedef struct _UNICODE_STRING {
+	USHORT  Length;
+	USHORT  MaximumLength;
+	PWSTR  Buffer;
+} UNICODE_STRING, * PUNICODE_STRING;
 
-typedef NTSTATUS(NTAPI *_NtDuplicateObject)(
-		HANDLE SourceProcessHandle,
-		HANDLE SourceHandle,
-		HANDLE TargetProcessHandle,
-		PHANDLE TargetHandle,
-		ACCESS_MASK DesiredAccess,
-		ULONG Attributes,
-		ULONG Options);
+typedef NTSTATUS(NTAPI* _NtQuerySystemInformation)(
+	ULONG SystemInformationClass,
+	PVOID SystemInformation,
+	ULONG SystemInformationLength,
+	PULONG ReturnLength
+	);
 
-typedef NTSTATUS(NTAPI *_NtQueryObject)(
-		HANDLE ObjectHandle,
-		ULONG ObjectInformationClass,
-		PVOID ObjectInformation,
-		ULONG ObjectInformationLength,
-		PULONG ReturnLength);
+typedef NTSTATUS(NTAPI* _NtDuplicateObject)(
+	HANDLE SourceProcessHandle,
+	HANDLE SourceHandle,
+	HANDLE TargetProcessHandle,
+	PHANDLE TargetHandle,
+	ACCESS_MASK DesiredAccess,
+	ULONG Attributes,
+	ULONG Options
+	);
 
-typedef struct _UNICODE_STRING
-{
-	USHORT Length;
-	USHORT MaximumLength;
-	PWSTR Buffer;
-} UNICODE_STRING, *PUNICODE_STRING;
+typedef NTSTATUS(NTAPI* _NtQueryObject)(
+	HANDLE ObjectHandle,
+	ULONG ObjectInformationClass,
+	PVOID ObjectInformation,
+	ULONG ObjectInformationLength,
+	PULONG ReturnLength
+	);
 
 typedef struct _SYSTEM_HANDLE
 {
@@ -61,13 +66,13 @@ typedef struct _SYSTEM_HANDLE
 	USHORT Handle;
 	PVOID Object;
 	ACCESS_MASK GrantedAccess;
-} SYSTEM_HANDLE, *PSYSTEM_HANDLE;
+} SYSTEM_HANDLE, * PSYSTEM_HANDLE;
 
 typedef struct _SYSTEM_HANDLE_INFORMATION
 {
 	ULONG HandleCount;
 	SYSTEM_HANDLE Handles[1];
-} SYSTEM_HANDLE_INFORMATION, *PSYSTEM_HANDLE_INFORMATION;
+} SYSTEM_HANDLE_INFORMATION, * PSYSTEM_HANDLE_INFORMATION;
 
 typedef enum _POOL_TYPE
 {
@@ -78,8 +83,7 @@ typedef enum _POOL_TYPE
 	NonPagedPoolCacheAligned,
 	PagedPoolCacheAligned,
 	NonPagedPoolCacheAlignedMustS
-} POOL_TYPE,
-		*PPOOL_TYPE;
+} POOL_TYPE, * PPOOL_TYPE;
 
 typedef struct _OBJECT_TYPE_INFORMATION
 {
@@ -105,7 +109,7 @@ typedef struct _OBJECT_TYPE_INFORMATION
 	POOL_TYPE PoolType;
 	ULONG PagedPoolUsage;
 	ULONG NonPagedPoolUsage;
-} OBJECT_TYPE_INFORMATION, *POBJECT_TYPE_INFORMATION;
+} OBJECT_TYPE_INFORMATION, * POBJECT_TYPE_INFORMATION;
 
 PVOID GetLibraryProcAddress(LPCSTR LibraryName, LPCSTR ProcName)
 {
@@ -118,10 +122,9 @@ PVOID GetLibraryProcAddress(LPCSTR LibraryName, LPCSTR ProcName)
 	return GetProcAddress(LibHandle, ProcName);
 }
 
-int main(void)
-{
+int main(void) {
 	// Initialise Buffer to get file path + name
-	TCHAR *buffer = (TCHAR *)malloc(MAX_PATH * sizeof(TCHAR));
+	TCHAR* buffer = (TCHAR*)malloc(MAX_PATH * sizeof(TCHAR));
 	if (!buffer || !*buffer)
 	{
 		printf("Something went wrong. Are you nearing your RAM limit?");
@@ -141,7 +144,7 @@ int main(void)
 	}
 
 	// Get the name of the file
-	TCHAR *pStrStart = _tcsrchr(buffer, '\\') + 1;
+	TCHAR* pStrStart = _tcsrchr(buffer, '\\') + 1;
 	if (!pStrStart)
 	{
 		printf("Something went wrong. Filename retrieval failed.");
@@ -149,7 +152,7 @@ int main(void)
 	}
 
 	// Copy the name of the file into a dedicated variable
-	TCHAR *pFileName = (TCHAR *)malloc(fNameLen * sizeof(TCHAR) + 1);
+	TCHAR* pFileName = (TCHAR*)malloc(fNameLen * sizeof(TCHAR) + 1);
 	if (pFileName == NULL)
 	{
 		printf("Something went wrong. Are you nearing your RAM limit?");
@@ -170,7 +173,7 @@ int main(void)
 	if (hProcessSnap == INVALID_HANDLE_VALUE)
 	{
 		printf("Something went wrong. There was an error in capturing system porcesses.");
-		return (SNAPFL);
+		return(SNAPFL);
 	}
 
 	// Define the process entries
@@ -189,11 +192,9 @@ int main(void)
 
 	DWORD pid = 0;
 	// Cycle through Process List
-	do
-	{
+	do {
 		// Get the appropriate Process PID
-		if (_tcsicmp(pe32.szExeFile, pFileName) == 0)
-		{
+		if (_tcsicmp(pe32.szExeFile, pFileName) == 0) {
 			pid = pe32.th32ProcessID;
 			break;
 		}
@@ -201,8 +202,7 @@ int main(void)
 	// Clean the snapshot object to prevent resource leakage
 	CloseHandle(hProcessSnap);
 
-	if (!pid)
-	{
+	if (!pid) {
 		_tprintf(TEXT("Process '%s' not found. Exiting..."), pFileName);
 		return SRCHFL;
 	}
@@ -212,19 +212,190 @@ int main(void)
 
 	// Retrieve the count of handles used by the process
 	int handleCount = retrieveHandleCount(OpenProcess(PROCESS_QUERY_INFORMATION |
-																												PROCESS_VM_READ,
-																										FALSE, pid));
+		PROCESS_VM_READ,
+		FALSE, pid));
 	if (!handleCount)
 		return err_code;
-	printf("The number of handles used is: %d.", handleCount);
+	printf("The number of handles used is: %d. The following file handles are in use:\n", handleCount);
 
 	// NT function imports
 	_NtQuerySystemInformation NtQuerySystemInformation =
-			(_NtQuerySystemInformation)GetLibraryProcAddress("ntdll.dll", "NtQuerySystemInformation");
+		(_NtQuerySystemInformation)GetLibraryProcAddress("ntdll.dll", "NtQuerySystemInformation");
 	_NtDuplicateObject NtDuplicateObject =
-			(_NtDuplicateObject)GetLibraryProcAddress("ntdll.dll", "NtDuplicateObject");
+		(_NtDuplicateObject)GetLibraryProcAddress("ntdll.dll", "NtDuplicateObject");
 	_NtQueryObject NtQueryObject =
-			(_NtQueryObject)GetLibraryProcAddress("ntdll.dll", "NtQueryObject");
+		(_NtQueryObject)GetLibraryProcAddress("ntdll.dll", "NtQueryObject");
+
+	NTSTATUS status;
+	PSYSTEM_HANDLE_INFORMATION handleInfo;
+	ULONG handleInfoSize = 0x10000;
+	HANDLE processHandle;
+	unsigned int i;
+
+	if (!(processHandle = OpenProcess(PROCESS_DUP_HANDLE, FALSE, pid))) {
+		printf("Could not open PID %d! (Don't try to open a system process.)\n", pid);
+		return 1;
+	}
+
+	handleInfo = (PSYSTEM_HANDLE_INFORMATION)malloc(handleInfoSize);
+	if (!handleInfo)
+	{
+		printf("Memory allocation failed.");
+		return MEMFL;
+	}
+
+	// NtQuerySystemInformation won't give us the correct buffer size,
+	//  so we guess by doubling the buffer size.
+	while ((status = NtQuerySystemInformation(
+		SystemHandleInformation,
+		handleInfo,
+		handleInfoSize,
+		NULL
+	)) == STATUS_INFO_LENGTH_MISMATCH)
+	{
+		PSYSTEM_HANDLE_INFORMATION tempHandle = (PSYSTEM_HANDLE_INFORMATION)realloc(handleInfo, handleInfoSize *= 2);
+		if (!tempHandle)
+		{
+			printf("Memory allocation failed.");
+			return MEMFL;
+		}
+		handleInfo = tempHandle;
+	}
+
+	// NtQuerySystemInformation stopped giving us STATUS_INFO_LENGTH_MISMATCH.
+	if (!NT_SUCCESS(status)) {
+		printf("NtQuerySystemInformation failed!\n");
+		free(handleInfo);
+		return 1;
+	}
+
+	for (i = 0; i < handleInfo->HandleCount; i++) {
+		SYSTEM_HANDLE handle = handleInfo->Handles[i];
+		HANDLE dupHandle = NULL;
+		POBJECT_TYPE_INFORMATION objectTypeInfo;
+		PVOID objectNameInfo;
+		UNICODE_STRING objectName;
+		ULONG returnLength;
+
+		// Check if this handle belongs to the PID the user specified.
+		if (handle.ProcessId != pid)
+			continue;
+
+		/* Skip handles with the following access codes as the next call
+		   to NtDuplicateObject() or NtQueryObject() might hang forever. */
+		if ((handle.GrantedAccess == 0x0012019f)
+			|| (handle.GrantedAccess == 0x001a019f)
+			|| (handle.GrantedAccess == 0x00120189)
+			|| (handle.GrantedAccess == 0x00100000)) {
+			continue;
+		}
+
+		// Duplicate the handle so we can query it.
+		if (!NT_SUCCESS(NtDuplicateObject(
+			processHandle,
+			(void*)handle.Handle,
+			GetCurrentProcess(),
+			&dupHandle,
+			0,
+			0,
+			0
+		))) {
+			//printf("[%#x] Error!\n", handle.Handle);
+			continue;
+		}
+
+		PWSTR fileType = TEXT("File");
+		// Query the object type.
+		objectTypeInfo = (POBJECT_TYPE_INFORMATION)malloc(0x1000);
+		NTSTATUS tempObjState = NtQueryObject(
+			dupHandle,
+			ObjectTypeInformation,
+			objectTypeInfo,
+			0x1000,
+			NULL
+		);
+		if (!objectTypeInfo) return NULLPTR;
+		if (!NT_SUCCESS(tempObjState)) {
+			printf("[%#x] Error!\n", handle.Handle);
+			CloseHandle(dupHandle);
+			continue;
+		}
+		else if (_tcsicmp(objectTypeInfo->Name.Buffer, fileType) != 0)
+		{
+			CloseHandle(dupHandle);
+			continue;
+		}
+
+		objectNameInfo = malloc(0x1000);
+		if (!NT_SUCCESS(NtQueryObject(
+			dupHandle,
+			ObjectNameInformation,
+			objectNameInfo,
+			0x1000,
+			&returnLength
+		))) {
+			// Reallocate the buffer and try again.
+			PVOID tempNameInfo = realloc(objectNameInfo, returnLength);
+			if (!tempNameInfo)
+			{
+				printf("Memory allocation failed.");
+				return MEMFL;
+			}
+			objectNameInfo = tempNameInfo;
+			if (!NT_SUCCESS(NtQueryObject(
+				dupHandle,
+				ObjectNameInformation,
+				objectNameInfo,
+				returnLength,
+				NULL
+			))) {
+				// We have the type name, so just display that.
+				printf(
+					"[%#x] %.*S: (could not get name)\n",
+					handle.Handle,
+					objectTypeInfo->Name.Length / 2,
+					objectTypeInfo->Name.Buffer
+				);
+
+				free(objectTypeInfo);
+				free(objectNameInfo);
+				CloseHandle(dupHandle);
+				continue;
+			}
+		}
+		if (!objectNameInfo) return NULLPTR;
+
+		// Cast our buffer into an UNICODE_STRING.
+		objectName = *(PUNICODE_STRING)objectNameInfo;
+
+		// Print the information!
+		if (objectName.Length)
+		{
+			// The object has a name.
+			printf(
+				"[%#x]: %.*S\n",
+				handle.Handle,
+				objectName.Length / 2,
+				objectName.Buffer
+			);
+		}
+		else {
+			// Print something else.
+			printf(
+				"[%#x] %.*S: (unnamed)\n",
+				handle.Handle,
+				objectTypeInfo->Name.Length / 2,
+				objectTypeInfo->Name.Buffer
+			);
+		}
+
+		free(objectTypeInfo);
+		free(objectNameInfo);
+		CloseHandle(dupHandle);
+	}
+
+	free(handleInfo);
+	CloseHandle(processHandle);
 
 	// Stop halt
 	//while ('\n' != getchar());
@@ -236,7 +407,7 @@ int main(void)
 }
 
 // Validates if the input contains a path and extension
-int validateInput(TCHAR *input, int *len)
+int validateInput(TCHAR* input, int* len)
 {
 	int dotExists = 0;
 	int indexSlash = 0;
@@ -265,8 +436,8 @@ int PrintModules(DWORD processID)
 
 	// Get a handle to the process
 	hProcess = OpenProcess(PROCESS_QUERY_INFORMATION |
-														 PROCESS_VM_READ,
-												 FALSE, processID);
+		PROCESS_VM_READ,
+		FALSE, processID);
 
 	if (NULL == hProcess)
 		return 1;
